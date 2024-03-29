65f732b9e637b48831455e9efa81a8144c560f1b
behrouz-Zamani <zmani.behrouz@hotmail.com>
2024-03-30 01:15:38 +0330

    change to hock

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css'
 import Hello from "./Hello";
 import Timer from './Timer';
-class App extends React.Component
-{
-
-  constructor()
-  {
-    super();
-    this.state={
-      title : "سلام"
-    }
-  }
 
 
-handleSetTitle =()=>{
-  this.setState({
-    title : "دوستان من سلام"
-  })
-}
+// class App extends React.Component
+// {
 
-  render()
-  {
-    return(
-      <div className='main'>
-        <Hello title={this.state.title}/>
-        <Timer handleSetTitle={this.handleSetTitle}/>
-      </div>
-    )
-  }
+//   constructor()
+//   {
+//     super();
+//     this.state={
+//       title : "سلام"
+//     }
+//   }
+
+
+// handleSetTitle =()=>{
+//   this.setState({
+//     title : "دوستان من سلام"
+//   })
+// }
+
+//   render()
+//   {
+//     return(
+      // <div className='main'>
+      //   <Hello title={this.state.title}/>
+      //   <Timer handleSetTitle={this.handleSetTitle}/>
+      // </div>
+//     )
+//   }
+// }
+
+const App = () => {
+  const [title, setTitle] = useState("Hi there");
+  const handleSetTitle =() => {setTitle("به برنامه من خوش آمدید")}
+
+  return(
+    <div className='main'>
+    <Hello title={title}/>
+    <Timer handleSetTitle={handleSetTitle}/>
+  </div>
+  )
+  
 }
 
 export default App;
